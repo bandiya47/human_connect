bf711834ffad3ef9303106e31c15e3bee2b79f5c
admin <admin@DESKTOP-F0FFAA5>
2024-06-05 08:38:52 +0900

    6/4일 한거 올립니다.

--- Human_connect/src/main/java/dc/human/gbnb/humanConnect/dao/CenterRegDAO.java
+++ Human_connect/src/main/java/dc/human/gbnb/humanConnect/dao/CenterRegDAO.java
@@ -11,6 +11,7 @@ import java.util.List;
 import javax.sql.DataSource;
 
 import dc.human.gbnb.humanConnect.dto.CenterRegDTO;
+import dc.human.gbnb.humanConnect.dto.volunteerDetailDTO;
 
 public class CenterRegDAO {
    
@@ -98,7 +99,7 @@ public class CenterRegDAO {
 //      }
 //   }
 //   
-   public List listCenterReg(CenterRegDTO centerRegDTO) {
+   public void listCenterReg(CenterRegDTO centerRegDTO) {
       List list = new ArrayList();
       try {
 //         connDB();
@@ -109,34 +110,61 @@ public class CenterRegDAO {
 	               "gbnb"
 	               );
     	 String vTitle = centerRegDTO.getvTitle();
-         String query = "select U_ID,V_TITLE,V_START_DATE,V_END_DATE,V_START_TIME,V_LAST_TIME,V_RSTART_DATE,V_REND_DATE,V_WORKING_DAY,SERVICE_CODE,V_MAX_AMNT,V_REG_AMNT,V_STATE,V_UPLOAD_FILE_PATH,V_INFO from volunteer where V_TITLE=? ";
+    	 
+    	 System.out.println(vTitle);
+    	 
+    	 
+         String query = "select V_TITLE, V_START_DATE, V_END_DATE, V_START_TIME, V_LAST_TIME, V_RSTART_DATE, V_REND_DATE, V_WORKING_DAY, SERVICE_CODE, V_MAX_AMNT, V_REG_AMNT, V_UPLOAD_FILE_PATH, V_INFO from volunteer where V_TITLE = ? ";
          System.out.println("prepareStatement: "+query);
          pstmt = con.prepareStatement(query);
          pstmt.setString(1, vTitle);
          
          
-         ResultSet rs = pstmt.executeQuery(query);
-         while (rs.next()) {
-            String id = rs.getString("id");
-            String pwd = rs.getString("pwd");
-            String name = rs.getString("name");
-            String email = rs.getString("email");
-            Date joinDate = rs.getDate("joinDate");
-            CenterRegDTO vo = new CenterRegDTO();
-            vo.setId(id);
-            vo.setPwd(pwd);
-            vo.setName(name);
-            vo.setEmail(email);
-            vo.setJoinDate(joinDate);
-            list.add(vo);
-         }
+         ResultSet rs = pstmt.executeQuery();
+         rs.next();
+            String V_TITLE = rs.getString("V_TITLE");
+            String V_START_DATE = rs.getString("V_START_DATE");
+            String V_END_DATE = rs.getString("V_END_DATE");
+            String V_START_TIME = rs.getString("V_START_TIME");
+            String V_LAST_TIME = rs.getString("V_LAST_TIME");
+            String V_RSTART_DATE = rs.getString("V_RSTART_DATE");
+            String V_REND_DATE = rs.getString("V_REND_DATE");
+            String V_WORKING_DAY = rs.getString("V_WORKING_DAY");
+            int SERVICE_CODE = rs.getInt("SERVICE_CODE");
+            int V_MAX_AMNT = rs.getInt("V_MAX_AMNT");
+            int V_REG_AMNT = rs.getInt("V_REG_AMNT");
+            String V_UPLOAD_FILE_PATH = rs.getString("V_UPLOAD_FILE_PATH");
+            String V_INFO = rs.getString("V_INFO");
+            
+            
+            
+            System.out.println(V_START_DATE);
+            
+
+            centerRegDTO.setvTitle(V_TITLE);
+            centerRegDTO.setvStartDate(V_START_DATE);
+            centerRegDTO.setvEndDate(V_END_DATE);
+            centerRegDTO.setvStartTime(V_START_TIME);
+            centerRegDTO.setvLastTime(V_LAST_TIME);            
+            centerRegDTO.setvRStartDate(V_RSTART_DATE);
+            centerRegDTO.setvREndDate(V_REND_DATE);
+            centerRegDTO.setvWorkingDay(V_WORKING_DAY);
+            centerRegDTO.setvServiceCode(SERVICE_CODE);            
+            centerRegDTO.setvMaxAmnt(V_MAX_AMNT);
+            centerRegDTO.setvRegAmnt(V_REG_AMNT);
+            centerRegDTO.setvUploadFilePath(V_UPLOAD_FILE_PATH);
+            centerRegDTO.setvInfo(V_INFO);
+            
+            System.out.println(centerRegDTO.getvEndDate());
+            
+        
          rs.close();
          pstmt.close();
          con.close();
          } catch (Exception e) {
             e.printStackTrace();
          }
-      return list;
+ 
    }
 //   
 //   public boolean isExisted(CenterRegDTO CenterRegDTO) {
